df15f6318f33390fd21802729413740e81d133e7
EasinTanvir <easintanvir102@gmail.com>
2025-12-02 23:56:57 +0600

    added cors

--- RestApi/Program.cs
+++ RestApi/Program.cs
@@ -10,11 +10,27 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlite("Data Source=dev.db"));
 
+
+// Add CORS for Next.js
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowNextJs", policy =>
+    {
+        policy.WithOrigins("http://localhost:3000")
+              .AllowAnyHeader()
+              .AllowAnyMethod();
+    });
+});
+
+
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
+
+//  Enable CORS BEFORE controllers
+app.UseCors("AllowNextJs");
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
